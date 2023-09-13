c963de43b94e604bd50079da36a5e413767b1085
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-09-13 07:58:23 +0100

    Update getYear.js

--- src/utils/getYear.js
+++ src/utils/getYear.js
@@ -1,12 +1,14 @@
 export const getYear = (year) => new Date(year).getUTCFullYear();
 
-
 export const getUtcTime = (inputDate) => {
-  const localDate = new Date(inputDate);
-  const utcDate = new Date(localDate.getTime() + localDate.getTimezoneOffset() * 60000);
-  const isoString = utcDate.toISOString();
-  
-  return isoString;
+  const parts = inputDate.split('-');
+  const year = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1; // Month is 0-based in JavaScript
+  const day = parseInt(parts[2], 10);
+
+  const utcDate = new Date(Date.UTC(year, month, day));
+
+  const utcDateString = utcDate.toISOString().split('T')[0];
+
+  return utcDateString;
 }
-
-
