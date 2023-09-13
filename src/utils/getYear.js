d726a2081768c42c44d34ad277826403c2882363
Isiaq Ridwan Bukola <99425435+RB-Isiaq@users.noreply.github.com>
2023-09-13 07:38:14 +0100

    Update getYear.js

--- src/utils/getYear.js
+++ src/utils/getYear.js
@@ -1,10 +1,6 @@
 export const getYear = (year) => new Date(year).getUTCFullYear();
 
-export const getUtcTime = (year) => {
-  const utcDate = new Date(Date.UTC(year));
-  const isoString = utcDate.toISOString();
-  return isoString
-}
+
 export const getUtcTime = (inputDate) => {
   const localDate = new Date(inputDate);
   const utcDate = new Date(localDate.getTime() + localDate.getTimezoneOffset() * 60000);
